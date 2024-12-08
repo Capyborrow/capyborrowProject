14e895f089c0c203756c382f1b30500d05518469
valtosw <volodijbessarabov@knu.ua>
2024-12-08 21:14:17 +0200

    Add project files.

---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,20 @@
+using capyborrowProject.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+// For Testing. Will be replaced or deleted later 
+
+namespace TestProject
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Get_ReturnsHelloWorld()
+        {
+            var controller = new ValuesController();
+            var result = controller.Get() as OkObjectResult;
+
+            Assert.NotNull(result);
+            Assert.Equal("Hello, world!", result.Value);
+        }
+    }
+}
---
+++ capyborrowProject/Controllers/ValuesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+// For Testing. Will be replaced or deleted later
+
+namespace capyborrowProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class ValuesController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            return Ok("Hello, world!");
+        }
+    }
+}
