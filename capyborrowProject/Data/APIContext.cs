36db294794d31d2d946232f3e7591af9b839934f
valtosw <volodijbessarabov@knu.ua>
2024-12-12 17:08:20 +0200

    added an APIContext

---
+++ capyborrowProject/Data/APIContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using capyborrowProject.Models;
+
+namespace capyborrowProject.Data
+{
+    public class APIContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        public APIContext(DbContextOptions<APIContext> options) : base(options)
+        {
+        }
+    }
+}
