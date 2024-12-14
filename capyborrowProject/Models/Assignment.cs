a213018934dc9a881b723683140c606693ea5879
valtosw <volodijbessarabov@knu.ua>
2024-12-14 18:18:04 +0200

    added a task model.

---
+++ capyborrowProject/Models/Assignment.cs
@@ -0,0 +1,18 @@
+﻿namespace capyborrowProject.Models
+{
+    public class Assignment
+    {
+        public enum TaskStatus
+        {
+            InReview,
+            Evaluated,
+            PastDeadline
+        }
+
+        public int Id { get; set; }
+        public TaskStatus Status { get; set; }
+        public string? Description { get; set; }
+        public required Teacher Teacher { get; set; }
+
+    }
+}
