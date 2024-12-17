39a637b6906f13aee258a61fba301ba4e352b369
valtosw <volodijbessarabov@knu.ua>
2024-12-17 17:43:29 +0200

    ....

--- capyborrowProject/Controllers/APIController.cs
+++ capyborrowProject/Controllers/APIController.cs
@@ -10,7 +10,7 @@ namespace capyborrowProject.Controllers
     [ApiController]
     public class APIController : ControllerBase
     {
-        private readonly APIContext _context;
+        private readonly APIContext _context; 
 
         public APIController(APIContext context)
         {
