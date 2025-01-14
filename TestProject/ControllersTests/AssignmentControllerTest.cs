94323a9cbabb6033f0dbdc1ef69966a9b330d101
YanOleks <yanytskyi.oleksandr@gmail.com>
2025-01-14 23:04:06 +0200

    Some code quality improvement: Creation of class instances delegated to factories

--- TestProject/ControllersTests/AssignmentControllerTest.cs
+++ TestProject/ControllersTests/AssignmentControllerTest.cs
@@ -1,9 +1,9 @@
 ﻿using capyborrowProject.Controllers;
 using capyborrowProject.Data;
 using capyborrowProject.Models;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
 
 namespace capyborrowTest.ControllersTests
 {
@@ -16,11 +16,10 @@ namespace capyborrowTest.ControllersTests
         public void SetUp()
         {
             var options = new DbContextOptionsBuilder<APIContext>()
-            .UseInMemoryDatabase(databaseName: "TestDatabase")
-            .Options;
+                .UseInMemoryDatabase("TestDatabase")
+                .Options;
 
             Context = new APIContext(options);
-
             SeedDatabase();
         }
 
@@ -32,238 +31,130 @@ namespace capyborrowTest.ControllersTests
         }
 
         [Test]
-        async public Task GetAllAssignments_ShouldReturnOk_WhenDataExists()
+        public async Task GetAllAssignments_ShouldReturnOk_WhenDataExists()
         {
-            // Arrange
             var controller = new AssignmentController(Context);
-
-            // Act
             var result = await controller.GetAllAssignments();
 
-            // Assert
-
             Assert.That(result.Result, Is.InstanceOf<OkObjectResult>());
-
             var okResult = result.Result as OkObjectResult;
             Assert.That(okResult, Is.Not.Null);
             Assert.That(okResult.StatusCode, Is.EqualTo(200));
-
-
-            var returnedAssignments = okResult.Value as List<Assignment>;
-            Assert.That(returnedAssignments, Is.Not.Null);
-
-            //should check if returned values are equal but with less hardcoding
-            //Assert.That(returnedAssignments.Count, Is.EqualTo(2));
-            //Assert.That("Assignment 1", Is.EqualTo(returnedAssignments[0].title));
+            var assignments = (IEnumerable<Assignment>)okResult.Value!;
+            Assert.That(assignments, Is.Not.Null);
         }
 
         [Test]
-        async public Task GetAssignment_ShouldReturnOk_WhenIndexInRange()
+        public async Task GetAssignment_ShouldReturnOk_WhenIndexInRange()
         {
-            // Arrange
             var controller = new AssignmentController(Context);
-
-            // Act
             var result = await controller.GetAssignment(1);
 
-            // Assert
-
             Assert.That(result.Result, Is.InstanceOf<OkObjectResult>());
-
             var okResult = result.Result as OkObjectResult;
             Assert.That(okResult, Is.Not.Null);
             Assert.That(okResult.StatusCode, Is.EqualTo(200));
-
-
-            var returnedAssignment = okResult.Value as Assignment;
-            Assert.That(returnedAssignment, Is.Not.Null);
-
-            //should check if returned values are equal but with less hardcoding
-            //Assert.That(returnedAssignments.Count, Is.EqualTo(2));
-            Assert.That(returnedAssignment.Title, Is.EqualTo("Assignment 1"));
+            var assignment = (Assignment)okResult.Value!;
+            Assert.That(assignment.Title, Is.EqualTo("Test Assignment 1"));
         }
 
         [Test]
-        async public Task GetAssignment_ShouldReturnNotFound_WhenIndexOutOfRange()
+        public async Task GetAssignment_ShouldReturnNotFound_WhenIndexOutOfRange()
         {
-            // Arrange
             var controller = new AssignmentController(Context);
-
-            // Act
             var result = await controller.GetAssignment(-1);
 
-            // Assert
-
             Assert.That(result.Result, Is.InstanceOf<NotFoundResult>());
-
-            var notFoundResult = result.Result as NotFoundResult;
-            Assert.That(notFoundResult?.StatusCode, Is.EqualTo(404));
         }
 
         [Test]
-        async public Task PostAssignment_ShouldReturnOk_WhenDataIsValid()
+        public async Task PostAssignment_ShouldReturnOk_WhenDataIsValid()
         {
-            // Arrange
             var controller = new AssignmentController(Context);
-            //should be put to datasource
-            Assignment assignment = new()
-            {
-                Title = "Test Assignment",
-                Status = Assignment.TaskStatus.ToDo,
-                DateAssigned = DateTime.UtcNow,
-                Lesson = new()
-                {
-                    Attendances = new List<Attendance>(),
-                    Date = DateTime.UtcNow,
-                    Group = new()
-                    {
-                        Name = "",
-                        Students = [],
-                    },
-                    Importance = Lesson.LessonImportance.Test,
-                    Location = "",
-                    Subject = new()
-                    {
-                        Teacher = new()
-                        {
-                            Email = "",
-                            FirstName = "",
-                            LastName = "",
-                            Notifications = [],
-                            PasswordHash = "",
-                            Role = 1,
-                            Subjects = [],
-                        },
-                        Title = "",
-                    },
-                    Type = Lesson.LessonType.Lecture,
-                },
-                Students = [],
-            };
+            var assignment = AssignmentFactory.CreateValidAssignment();
 
-            // Act
             var result = await controller.PostAssignment(assignment);
 
-            // Assert
             Assert.That(result.Result, Is.InstanceOf<OkObjectResult>());
-
-            var okObjectResult = result.Result as OkObjectResult;
-            Assert.That(okObjectResult?.StatusCode, Is.EqualTo(200));
-
+            var okResult = result.Result as OkObjectResult;
+            Assert.That(okResult, Is.Not.Null);
+            Assert.That(okResult.StatusCode, Is.EqualTo(200));
+            var createdAssignment = (Assignment)okResult.Value!;
+            Assert.That(createdAssignment.Title, Is.EqualTo("Test Assignment"));
         }
 
         [Test]
-        async public Task DeleteAssignment_ShouldReturnNoContent_IndexInRange()
+        public async Task DeleteAssignment_ShouldReturnNoContent_WhenIndexInRange()
         {
-            // Arrange
             var controller = new AssignmentController(Context);
-
-            // Act
             var result = await controller.DeleteAssignment(1);
 
-            // Assert
             Assert.That(result, Is.InstanceOf<NoContentResult>());
-
-            var noContentResult = result as NoContentResult;
-            Assert.That(noContentResult?.StatusCode, Is.EqualTo(204));
-
         }
 
         [Test]
-        async public Task DeleteAssignment_ShouldReturnNotFound_IndexOutOfRange()
+        public async Task DeleteAssignment_ShouldReturnNotFound_WhenIndexOutOfRange()
         {
-            // Arrange
             var controller = new AssignmentController(Context);
-
-            // Act
             var result = await controller.DeleteAssignment(-1);
 
-            // Assert
             Assert.That(result, Is.InstanceOf<NotFoundResult>());
-
-            var notFoundResult = result as NotFoundResult;
-            Assert.That(notFoundResult?.StatusCode, Is.EqualTo(404));
-
         }
 
-
-        //should be added to DataSource
         private void SeedDatabase()
         {
-
-
             Context.Assignments.AddRange(
-                new Assignment
-                {
-                    Title = "Test Assignment 1",
-                    Status = Assignment.TaskStatus.ToDo,
-                    DateAssigned = DateTime.UtcNow,
-                    Lesson = new()
-                    {
-                        Attendances = new List<Attendance>(),
-                        Date = DateTime.UtcNow,
-                        Group = new()
-                        {
-                            Name = "",
-                            Students = [],
-                        },
-                        Importance = Lesson.LessonImportance.Test,
-                        Location = "",
-                        Subject = new()
-                        {
-                            Teacher = new()
-                            {
-                                Email = "",
-                                FirstName = "",
-                                LastName = "",
-                                Notifications = [],
-                                PasswordHash = "",
-                                Role = 1,
-                                Subjects = [],
-                            },
-                            Title = "",
-                        },
-                        Type = Lesson.LessonType.Lecture,
-                    },
-                    Students = [],
-                },
-                new Assignment
-                {
-                    Title = "Test Assignment 2",
-                    Status = Assignment.TaskStatus.ToDo,
-                    DateAssigned = DateTime.UtcNow,
-                    Lesson = new()
-                    {
-                        Attendances = new List<Attendance>(),
-                        Date = DateTime.UtcNow,
-                        Group = new()
-                        {
-                            Name = "",
-                            Students = [],
-                        },
-                        Importance = Lesson.LessonImportance.Test,
-                        Location = "",
-                        Subject = new()
-                        {
-                            Teacher = new()
-                            {
-                                Email = "",
-                                FirstName = "",
-                                LastName = "",
-                                Notifications = [],
-                                PasswordHash = "",
-                                Role = 1,
-                                Subjects = [],
-                            },
-                            Title = "",
-                        },
-                        Type = Lesson.LessonType.Lecture,
-                    },
-                    Students = [],
-                }
+                AssignmentFactory.CreateAssignment(1, "Test Assignment 1"),
+                AssignmentFactory.CreateAssignment(2, "Test Assignment 2")
             );
 
             Context.SaveChanges();
         }
+    }
+
+    internal static class AssignmentFactory
+    {
+        public static Assignment CreateValidAssignment() => new()
+        {
+            Title = "Test Assignment",
+            Status = Assignment.TaskStatus.ToDo,
+            DateAssigned = DateTime.UtcNow,
+            Lesson = CreateLesson(),
+            Students = []
+        };
+
+        public static Assignment CreateAssignment(int id, string title) => new()
+        {
+            Id = id,
+            Title = title,
+            Status = Assignment.TaskStatus.ToDo,
+            DateAssigned = DateTime.UtcNow,
+            Lesson = CreateLesson(),
+            Students = []
+        };
+
+        private static Lesson CreateLesson() => new()
+        {
+            Attendances = [],
+            Date = DateTime.UtcNow,
+            Group = new Group { Name = "", Students = [] },
+            Importance = Lesson.LessonImportance.Test,
+            Location = "",
+            Subject = new Subject
+            {
+                Teacher = new Teacher
+                {
+                    Email = "",
+                    FirstName = "",
+                    LastName = "",
+                    Notifications = [],
+                    PasswordHash = "",
+                    Role = 1,
+                    Subjects = []
+                },
+                Title = ""
+            },
+            Type = Lesson.LessonType.Lecture
+        };
     }
 }
