b24a0af8d9a0ecd5dc6f8936f230057695b12f92
valtosw <volodijbessarabov@knu.ua>
2025-02-16 19:34:39 +0200

    update migrations

---
+++ capyborrowProject/Migrations/20250216173013_AddedAttendanceMigration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace capyborrowProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedAttendanceMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Attendance",
+                table: "Lessons",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Attendance",
+                table: "Lessons");
+        }
+    }
+}
