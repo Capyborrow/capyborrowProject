70151ed3aa2c6f7ef55debbea7dbdbf64cacd99d
Владислав Гладкий <vlad.v.gladkiy@gmail.com>
2025-02-20 08:57:07 +0200

    add Lesson import from CSV file, enums perceived as int

---
+++ capyborrowProject/Models/CsvFilesModels/LessonCsvDto.cs
@@ -0,0 +1,13 @@
+﻿namespace capyborrowProject.Models.CsvFilesModels
+{
+    public class LessonCsvDto
+    {
+        public string Location { get; set; } = string.Empty;
+        public DateTime Date { get; set; }
+        public int Type { get; set; }
+        public int Attendance { get; set; }
+        public string SubjectName { get; set; } = string.Empty;
+        public string TeacherEmail { get; set; } = string.Empty;
+        public string GroupName { get; set; } = string.Empty;
+    }
+}
