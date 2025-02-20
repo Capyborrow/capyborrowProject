51b0e3e27016570f81eb7be52047159101c24fe0
Владислав Гладкий <vlad.v.gladkiy@gmail.com>
2025-02-20 09:18:51 +0200

    add Group import (creating new group and matching students from db by email)

---
+++ capyborrowProject/Models/CsvFilesModels/GroupCsvMap.cs
@@ -0,0 +1,12 @@
+﻿namespace capyborrowProject.Models.CsvFilesModels;
+
+using CsvHelper.Configuration;
+
+public class GroupCsvMap : ClassMap<GroupCsvDto>
+{
+    public GroupCsvMap()
+    {
+        Map(m => m.GroupName).Name("GroupName");
+        Map(m => m.StudentEmail).Name("StudentEmail");
+    }
+}
