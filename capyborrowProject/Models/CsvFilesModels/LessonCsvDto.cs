0f6e3adbb7d98e735dd2b9f0a61a1d06669eed28
Владислав Гладкий <vlad.v.gladkiy@gmail.com>
2025-02-24 19:45:26 +0200

    redo lesson import method

--- capyborrowProject/Models/CsvFilesModels/LessonCsvDto.cs
+++ capyborrowProject/Models/CsvFilesModels/LessonCsvDto.cs
@@ -6,6 +6,7 @@
         public string? Link { get; set; }
         public DateTime Date { get; set; }
         public int Type { get; set; }
+        public int Status { get; set; }
         public string SubjectName { get; set; } = string.Empty;
         public string TeacherEmail { get; set; } = string.Empty;
         public string GroupName { get; set; } = string.Empty;
