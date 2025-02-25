c9ba52adfd859dec2024a46168d10fdd50b8b7a3
valtosw <volodijbessarabov@knu.ua>
2025-02-25 21:23:48 +0200

    write GetUserById method

---
+++ capyborrowProject/Controllers/ApplicationUserController.cs
@@ -0,0 +1,21 @@
+﻿using capyborrowProject.Data;
+using capyborrowProject.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace capyborrowProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicationUserController(ApplicationDbContext context) : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ApplicationUser>> GetUserById(string id)
+        {
+            var user = await context.Users
+                .FirstOrDefaultAsync(u => u.Id == id);
+            return user is null ? NotFound() : Ok(user);
+        }
+    }
+}
