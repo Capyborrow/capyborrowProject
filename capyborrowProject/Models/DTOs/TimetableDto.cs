56e37477f5951f74bf7f789d2753ca40649a017c
valtosw <volodijbessarabov@knu.ua>
2025-03-14 11:10:15 +0200

    move dtos folder to models folder

---
+++ capyborrowProject/Models/DTOs/TimetableDto.cs
@@ -0,0 +1,49 @@
+﻿using capyborrowProject.Models;
+
+namespace capyborrowProject.Models.DTOs
+{
+    public class TimetableDto
+    {
+        /*public enum AttendanceType
+        {
+            Attended,
+            Skipped,
+            Excused,
+            Unknown,
+            Cancelled
+        }
+
+        public enum AssignmentStatusEnum
+        {
+            Due,
+            Overdue,     
+            Submitted,   
+            Graded,      
+            Expired       
+        }*/
+        public enum CommentStatusEnum
+        {
+            Read,
+            Unread
+        }
+        /*public enum LessonType
+        {
+            Lecture,
+            Practice,
+            Seminar,
+            Consultation,
+            Exam
+        }*/
+        public required DateTime Date { get; set; }
+        public required string SubjectName { get; set; }
+        public required string TeacherName { get; set; }
+        public required string TeacherAvatar { get; set; }
+        public string? Link { get; set; }
+        public string? Room { get; set; }
+        public required LessonType Type { get; set; }
+        public AttendanceType LessonStatus { get; set; }
+        public AssignmentStatus? AssignmentStatus { get; set; }
+        public required CommentStatusEnum IsRead { get; set; }
+
+    }
+}
