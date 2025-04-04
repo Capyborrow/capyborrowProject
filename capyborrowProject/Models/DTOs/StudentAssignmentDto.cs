6e989703533f114a47f2528adcea9d5ef8733595
valtosw <volodijbessarabov@knu.ua>
2025-04-04 16:45:37 +0300

    add id field to timetable dto. add needed dtos for an endpoint.

---
+++ capyborrowProject/Models/DTOs/StudentAssignmentDto.cs
@@ -0,0 +1,11 @@
+﻿namespace capyborrowProject.Models.DTOs
+{
+    public class StudentAssignmentDto
+    {
+        public float? Score { get; set; }
+        public DateTime? SubmittedAt { get; set; }
+        public AssignmentStatus? Status { get; set; }
+
+        public List<SubmissionFileDto> Submissions { get; set; } = [];
+    }
+}
