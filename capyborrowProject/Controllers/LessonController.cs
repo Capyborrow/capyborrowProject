606a7719a6687961fd1968c18f80556a9f3da545
valtosw <volodijbessarabov@knu.ua>
2025-04-04 16:46:41 +0300

    add GetAssignmentsByLessonAndStudent endpoint

--- capyborrowProject/Controllers/LessonController.cs
+++ capyborrowProject/Controllers/LessonController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using capyborrowProject.Models;
 using capyborrowProject.Data;
+using capyborrowProject.Models.DTOs;
 
 namespace capyborrowProject.Controllers
 {
@@ -10,6 +11,46 @@ namespace capyborrowProject.Controllers
     [ApiController]
     public class LessonController (ApplicationDbContext context) : ControllerBase
     {
+        [HttpGet("getAssignmentsForLesson/{lessonId}/{studentId}")]
+        public async Task<ActionResult<IEnumerable<AssignmentDto>>> GetAssignmentsByLessonAndStudent(int lessonId, string studentId)
+        {
+            var assignments = await context.Assignments
+                .Where(a => a.LessonId == lessonId)
+                .Include(a => a.AssignmentFiles)
+                .Include(a => a.StudentAssignments.Where(sa => sa.StudentId == studentId))
+                    .ThenInclude(sa => sa.SubmissionFiles)
+                .ToListAsync();
+
+            var result = assignments.Select(a => new AssignmentDto
+            {
+                Id = a.Id,
+                Name = a.Name,
+                Description = a.Description,
+                CreatedDate = a.CreatedDate,
+                MaxScore = a.MaxScore,
+                Attachments = [.. a.AssignmentFiles.Select(af => new AssignmentFileDto
+                {
+                    Id = af.Id,
+                    FileName = af.FileName,
+                    FileUrl = af.FileUrl
+                })],
+                StudentAssignment = a.StudentAssignments.FirstOrDefault() != null ? new StudentAssignmentDto
+                {
+                    Score = a.StudentAssignments.First().Score,
+                    SubmittedAt = a.StudentAssignments.First().SubmittedAt,
+                    Status = a.StudentAssignments.First().ComputedStatus,
+                    Submissions = [.. a.StudentAssignments.First().SubmissionFiles.Select(sf => new SubmissionFileDto
+                    {
+                        Id = sf.Id,
+                        FileName = sf.FileName,
+                        FileUrl = sf.FileUrl
+                    })]
+                } : null
+            });
+
+            return Ok(result);
+        }
+
         [HttpGet("{id}")]
         public async Task<ActionResult<Lesson>> GetLesson(int id)
         {
