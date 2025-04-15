9340d746ad166917ec8007ced77bc4d554c9b3ae
valtosw <volodijbessarabov@knu.ua>
2025-04-15 21:13:17 +0300

    add new migrations

--- capyborrowProject/Migrations/20250415180603_NoCommentsMigration.Designer.cs
+++ capyborrowProject/Migrations/20250415180603_NoCommentsMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using capyborrowProject.Data;
 
@@ -11,9 +12,11 @@ using capyborrowProject.Data;
 namespace capyborrowProject.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250415180603_NoCommentsMigration")]
+    partial class NoCommentsMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
