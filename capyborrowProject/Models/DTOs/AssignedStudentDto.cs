87e200a91aa88eaf3f8ba4a0da7414deddf543f5
valtosw <volodijbessarabov@knu.ua>
2025-04-18 20:09:37 +0300

    add AssignedStudentDto class to DTOs namespace

---
+++ capyborrowProject/Models/DTOs/AssignedStudentDto.cs
@@ -0,0 +1,9 @@
+﻿namespace capyborrowProject.Models.DTOs
+{
+    public class AssignedStudentDto
+    {
+        public required string Id { get; set; }
+        public required string FullName { get; set; }
+        public string? ProfilePicture { get; set; }
+    }
+}
