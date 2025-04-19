d9c44b61c93ff585dbfb4156048d3ca7cef0060b
valtosw <volodijbessarabov@knu.ua>
2025-04-19 22:05:22 +0300

    CPB-122: add a dto for returning only needed data

---
+++ capyborrowProject/Models/DTOs/UserProfileDto.cs
@@ -0,0 +1,10 @@
+﻿namespace capyborrowProject.Models.DTOs
+{
+    public class UserProfileDto
+    {
+        public required string FirstName { get; set; }
+        public string? MiddleName { get; set; }
+        public required string LastName { get; set; }
+        public string? ProfilePicture { get; set; }
+    }
+}
